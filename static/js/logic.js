ebdb9f425fb78bedfa2c3d2c075e6f4dfa038a85
Evan.R <evanarr@gmail.com>
2020-10-13 18:36:40 -0700

    got the tile layer working

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,56 @@
-//following leaflet quickstart guide
-//initialize map
-// var mymap = L.map('map').setView([37.80, 122.27], 13);
 
-//create geojson layer
-var geoLayer = L.geoJSON().addTo(map);
-geoLayer.addData('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson')
+
+const API_KEY = "pk.eyJ1IjoiZXZhbi1yZWV2ZXMiLCJhIjoiY2tnOG9qNmk1MDA5dzJ5cWQyZnU1eDNraiJ9.cizxjMqYDChKKheSJSy2fA";
+
+// Creating map object
+var myMap = L.map("map", {
+    center: [37.80, -122.27],
+    zoom: 11
+  });
+  
+  // create tile layer 
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+  // Store API query variables
+  var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
+  var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
+  var complaint = "&complaint_type=Rodent";
+  var limit = "&$limit=10000";
+  
+  // // Assemble API query URL
+  // var url = baseURL + date + complaint + limit;
+  
+  // // Grab the data with d3
+  // d3.json(url, function(response) {
+  
+  //   // Create a new marker cluster group
+  //   var markers = L.markerClusterGroup();
+  
+  //   // Loop through data
+  //   for (var i = 0; i < response.length; i++) {
+  
+  //     // Set the data location property to a variable
+  //     var location = response[i].location;
+  
+  //     // Check for location property
+  //     if (location) {
+  
+  //       // Add a new marker to the cluster group and bind a pop-up
+  //       markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+  //         .bindPopup(response[i].descriptor));
+  //     }
+  
+  //   }
+  
+  //   // Add our marker cluster layer to the map
+  //   myMap.addLayer(markers);
+  
+  // });
+  
